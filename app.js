4b708e6b0671cb86f85f8aa7e7ebf5c88c4f3175
JD Angerhofer <jd.angerhofer@gmail.com>
2016-02-24 22:20:55 -0500

    Force SSL Connection
    Signed-off-by: JD Angerhofer <jd.angerhofer@gmail.com>

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('forceSSLOptions', {
+  enable301Redirects: true,
+  trustXFPHeader: false,
+  httpsPort: 443,
+  sslRequiredMessage: 'SSL Required.'
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
