2f3141f277b120f1bd3dbb9fd01163938abd56dc
JD Angerhofer <jd.angerhofer@gmail.com>
2016-04-07 22:28:42 -0400

    Redirect to Fill Form when Logged In
    Redirect to /fill when visiting root and already logged in
    
    Signed-off-by: JD Angerhofer <jd.angerhofer@gmail.com>

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var session = require('express-session')
 
-var routes = require('./routes/index');
+var routes = require('./routes/index')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.set('forceSSLOptions', {
   enable301Redirects: true,
   trustXFPHeader: false,
   httpsPort: 443,
   sslRequiredMessage: 'SSL Required.'
-});
+})
 
 function checkAuth(req, res, next) {
-  console.log('checkAuth ' + req.url);
+  console.log('checkAuth ' + req.url)
 
   // don't serve /secure to those not logged in
   // you should add to this list, for each and every secure url
   if (req.url === '/fill' && (!req.session || !req.session.authenticated)) {
     res.render('index', {
       error: "Please login first."
-    });
-    return;
+    })
+    return
   }
-  next();
+  if (req.url === '/' && req.session.authenticated) {
+    res.redirect('/fill')
+    return
+  }
+  next()
 }
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(session({
   secret: 'G4MnTIgi0ocNqHqpPCUhvttR9Q/6GZtWWh92tR2keiupAzEMpX6f24s1LfZw3s4408QiR8aCWTPyYHT9'
 }))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
-}));
-app.use(checkAuth);
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+}))
+app.use(checkAuth)
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
+app.use('/', routes)
 
-module.exports = app;
+module.exports = app
